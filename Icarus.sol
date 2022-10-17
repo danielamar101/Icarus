c76e3beab198e0d0c9c397fdceeab2ed6b4f301c
danielamar101 <danielamarble123456@gmail.com>
2022-10-17 12:40:36 -1000

    Non-tested version that should work. Next steps is to manually test with sample screenshot array

--- Icarus.sol
+++ Icarus.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.1;
+pragma solidity ^0.8.1;
+
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -11,7 +12,6 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 contract DreamPass is ERC721, Ownable {
     using SafeMath for uint;
 
-    uint256 DREAM_PASS_ID = 0;
     uint256 public maxQuantity = 500;
     uint256 public mintCounter = 0;
 
@@ -20,15 +20,6 @@ contract DreamPass is ERC721, Ownable {
     string public contractURIString;
 
     event AnnounceMint(address minter, uint id, uint count);
-
-    //Create authentication around a comic book
-    modifier onlyComic(address _address){
-        if(comicAddress == _address){
-            _;
-        } else{
-            revert("The comic must call this method!");
-        }
-    }
 
     constructor(string memory _contractURIString, string memory _name, string memory _symbol) ERC721(_name,_symbol) {
        
@@ -43,13 +34,34 @@ contract DreamPass is ERC721, Ownable {
 
         mintCounter = SafeMath.add(mintCounter,1);
 
-        emit AnnounceMint(msg.sender,DREAM_PASS_ID, mintCounter);
+        emit AnnounceMint(msg.sender,mintCounter, mintCounter);
+    }
+
+    // Retuns metadata of the comic collection, also for OpenSeas compatability
+    function contractURI() public view returns (string memory) {
+        return string(abi.encodePacked(
+                contractURIString,
+                "info.json")
+            );
+    }
+
+    // Retuns metadata of each token, also for OpenSeas compatability
+    // Since all dreampasses will be the same, same value is returned now
+    function tokenURI(uint256 _tokenId) public override view returns(string memory){
+        require(_tokenId < mintCounter, "Invalid token ID!");
+
+        return string(
+            abi.encodePacked(
+                contractURIString,
+                Strings.toString(0),
+                ".json"
+            )
+        );
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
         return contractURIString;
     }
-
 }
 
 contract ComicKey is ERC1155, Ownable {
@@ -74,122 +86,61 @@ contract ComicKey is ERC1155, Ownable {
     mapping(uint256 => address) public idToMomentAddress;
     mapping(address => uint256) public momentAddressToid;
 
-    //Temporary
-    mapping(address => uint256) addressToQuantity;
-
     modifier onlyChildren(address _address){
-        //Doubly linked list for O(1) access
+        //Doubly linked for O(1) access
         if(idToComicAddress[comicAddressToId[_address]] != address(0)){
             _;
         } 
     }
    
-    constructor(address _dreampassAddress, address TESTER, uint256 _priceToMintDiscounted, uint256 _priceToMintFull, uint256 _momentPrice, string memory _name, string memory _symbol, string memory _contractURIString) 
+    constructor(string memory _contractURIString) 
     ERC1155(_contractURIString) {
 
-        //Deploy a new comic book contract
-        Comic newComic = new Comic(keyIdCount, _priceToMintDiscounted, _priceToMintFull, address(this), _name, _symbol, _contractURIString);
+        contractURIString = _contractURIString;
+    }
 
+    function setDreamPassAddress(address _address) public onlyOwner {
+        dreampassAddress = _address;
+    }
+
+    // Deploy comic, moment and airdrop new comic key
+    // @requires: allDreampassOwners and allDreampassQuantities to be of the same length and obtained from a snapshot
+    // 1. Instantiate a new comic book
+    // 2. Instantiate a new mintable moment
+    // 3. Airdrop by:
+    //         1. minting <quantity> new tokens of id COMIC_KEY_ID
+    //         2. transfer all these newly minted tokens to the addresses in the passed in array 
+    function deployRound(address[] memory allDreampassOwners,uint256[] memory allDreampassQuantities, uint256 _priceToMintDiscounted, uint256 _priceToMintFull, uint256 _momentPrice, string memory _name, string memory _symbol, string memory _contractURI) public onlyOwner(){
+        require(allDreampassOwners.length == allDreampassQuantities.length, "Invalid size of airdrop arrays");
+        //TODO: Add more stringent input validation
+
+        Comic newComic = new Comic(keyIdCount, _priceToMintDiscounted, _priceToMintFull, address(this), _name, _symbol, _contractURI);
         address newComicAddress = address(newComic);
 
         //Add to comic record keeping
         idToComicAddress[keyIdCount] = newComicAddress;
         comicAddressToId[newComicAddress] = keyIdCount;
 
-        //Deploy a new mintable moment contract
-        Moment newMoment = new Moment(keyIdCount, _momentPrice, newComicAddress, _name, _symbol, _contractURIString);
-
+        //Create mintable moment
+        Moment newMoment = new Moment(keyIdCount, _momentPrice, address(newComic), _name, _symbol, _contractURI);
         address newMomentAddress = address(newMoment);
 
         //Add to moment record keeping
         idToMomentAddress[keyIdCount] = newMomentAddress;
         comicAddressToId[newMomentAddress];
 
-        //Sets the dreampass address to use as reference during snapshot
-        dreampassContract = DreamPass(_dreampassAddress);
-        dreampassAddress = _dreampassAddress;
+        //AIRDROP NEW COMIC KEYS
 
-        //DEV: TESTING ONLY
-        _mint(TESTER,keyIdCount,1,"");
+        // 1. Mint 500 
+        _mint(address(this),keyIdCount,STANDARD_COMIC_KEY_QUANTITY,"");
 
+        // 2. Transfer (Airdrop) comic keys to all dreampass holders
+        for(uint256 i = 0; i < allDreampassOwners.length; i++){
+            safeTransferFrom(address(this),allDreampassOwners[i],keyIdCount, allDreampassQuantities[i], "");
+        }
 
-
-        keyIdCount++;
-        contractURIString = _contractURIString;
+        keyIdCount = SafeMath.add(keyIdCount,1);
     }
-
-    function testShit() public view returns(address){
-        return dreampassContract.ownerOf(0);
-    }
-
-    // //Just to test snapshotting. Would probably make sense to have this off chain? Idk
-    // function snapshotDreampassState() public returns (address[] memory _allDreampassOwners, uint256[] memory _allDreampassQuantities){
-
-    //     address[] memory allDreampassOwners = new address[];
-    //     uint256[] memory allDreampassQuantities = new uint256[];
-    //     uint256 arrayCounter = 0;
-
-    //     //TODO: Kinda nasty, implement better
-    //     for(uint256 i = 0; i < dreampassContract.mintCounter(); i++){
-    //         address ownerAtIndex = dreampassContract.ownerOf(i);
-    //         if(addressToQuantity[ownerAtIndex] == 0){
-    //             allDreampassOwners[arrayCounter] = ownerAtIndex;
-    //             arrayCounter++;
-    //         }
-    //         addressToQuantity[ownerAtIndex]++;
-
-    //     }
-
-    //     for(uint256 i = 0; i < arrayCounter; i++){
-    //         allDreampassQuantities[i] = addressToQuantity[allDreampassOwners[i]];
-    //     }
-
-    //     return (allDreampassOwners, allDreampassQuantities);
-    // }
-
-
-    function setDreamPassAddress(address _address) public onlyOwner {
-        dreampassAddress = _address;
-    }
-
-    // This method will
-    // 1. Instantiate a new comic book
-    // 2. Instantiate a new mintable moment
-    // 3. Airdrop by:
-            // 1. minting <quantity> new tokens of id COMIC_KEY_ID
-            // 2. transfer all these newly minted tokens to the addresses in the passed in array 
-    // function deployRound(address[] memory allDreampassOwners,uint256[] memory allDreampassQuantities, uint256 _priceToMintDiscounted, uint256 _priceToMintFull, uint256 _momentPrice, string memory _name, string memory _symbol, string memory _contractURI) public onlyOwner(){
-    //     require(allDreampassOwners.length == allDreampassQuantities.length, "Invalid size of airdrop arrays");
-
-    //     Comic newComic = new Comic(keyIdCount, _priceToMintDiscounted, _priceToMintFull, address(this), _name, _symbol, _contractURI);
-
-    //     address newComicAddress = address(newComic);
-
-    //     //Add to comic record keeping
-    //     idToComicAddress[keyIdCount] = newComicAddress;
-    //     comicAddressToId[newComicAddress] = keyIdCount;
-
-    //     //Create mintable moment
-    //     Moment newMoment = new Moment(keyIdCount, _momentPrice, address(newComic), _name, _symbol, _contractURI);
-
-    //     address newMomentAddress = address(newMoment);
-
-    //     //Add to moment record keeping
-    //     idToMomentAddress[keyIdCount] = newMomentAddress;
-    //     comicAddressToId[newMomentAddress];
-
-    //     //AIRDROP NEW COMIC KEYS
-
-    //     //1. Mint 500 
-    //     _mint(address(this),keyIdCount,STANDARD_COMIC_KEY_QUANTITY,"");
-
-    //     //2. Transfer (Airdrop) comic keys to all dreampass holders
-    //     for(uint256 i = 0; i < allDreampassOwners.length; i++){
-    //         safeTransferFrom(address(this),allDreampassOwners[i],keyIdCount, allDreampassQuantities[i], "");
-    //     }
-
-    //     keyIdCount = SafeMath.add(keyIdCount,1);
-    // }
 
     function burn(address _address, uint256 _comicId) public onlyChildren(msg.sender){
         _burn(_address, _comicId, 1);
@@ -218,7 +169,7 @@ contract ComicKey is ERC1155, Ownable {
     }
 
     //Dev function
-    function setContractURI(string memory _contractURI) public {
+    function setContractURI(string memory _contractURI) public onlyOwner(){
         contractURIString = _contractURI;
     }
 
@@ -227,7 +178,7 @@ contract ComicKey is ERC1155, Ownable {
 
 contract Comic is ERC721, Ownable {
     using SafeMath for uint;
-    //TODO: Revisit storing comic ID as we can just have it link to a new URI
+    //TODO: Revisit storing comic ID as we can just have each contract link to a new URI
     uint256 public COMIC_ID;
     uint256 public mintCounter = 0;
     uint256 public maxQuantity = 700;
@@ -259,12 +210,13 @@ contract Comic is ERC721, Ownable {
         
         if(comicKeyContract.balanceOf(msg.sender,COMIC_ID) > 0){  //If person owns a comic key, mint at discounted rate
             require(msg.value == priceToMintDiscounted, "You did not send the right amount of eth to purchase a discounted comic!");
+            
             executeMintSequence();
 
-            //BURN COMIC KEY
+            // BURN COMIC KEY
             comicKeyContract.burn(msg.sender, COMIC_ID);
 
-        } else{ //user doesnt have comic key
+        } else{ //user doesnt have comic key, mint at full price
             require(fullPriceMintCountPerAddress[msg.sender] < 2, "You have already minted 2 of these comics already!");
             require(msg.value == priceToMintFull,"You did not send enough eth to purchase a full price comic!");
 
@@ -280,18 +232,30 @@ contract Comic is ERC721, Ownable {
         emit AnnounceMint(msg.sender,COMIC_ID);
     }
 
+    //Dev function
+    function setContractURI(string memory _contractURI) public onlyOwner(){
+        contractURIString = _contractURI;
+    }
+
+    // Retuns metadata of the comic collection, also for OpenSeas compatability
     function contractURI() public view returns (string memory) {
+        return string(abi.encodePacked(
+                contractURIString,
+                "info.json")
+            );
+    }
+
+    // Retuns metadata of each token, also for OpenSeas compatability
+    function tokenURI(uint256 _tokenId) public override view returns(string memory){
+        require(_tokenId < mintCounter, "Invalid token ID!");
+
         return string(
             abi.encodePacked(
                 contractURIString,
-                "info.json"
+                Strings.toString(_tokenId),
+                ".json"
             )
         );
-    }
-
-    //Dev function
-    function setContractURI(string memory _contractURI) public {
-        contractURIString = _contractURI;
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
@@ -307,10 +271,9 @@ contract Moment is ERC721, Ownable {
     uint256 public mintCounter = 0;
     uint256 public maxQuantity = 50;
     uint256 public priceToMint;
-    address public omicAddress;
 
     Comic comicContract;
-    address comicAddress;
+    address public comicAddress;
 
     string contractURIString;
 
@@ -338,13 +301,35 @@ contract Moment is ERC721, Ownable {
         emit AnnounceMint(msg.sender,MOMENT_ID);
     }
 
+    //Dev function
+    function setContractURI(string memory _contractURI) public onlyOwner(){
+        contractURIString = _contractURI;
+    }
+
+    // Retuns metadata of the comic collection, also for OpenSeas compatability
+    function contractURI() public view returns (string memory) {
+        return string(abi.encodePacked(
+                contractURIString,
+                "info.json")
+            );
+    }
+
+    // Retuns metadata of each token, also for OpenSeas compatability
+    function tokenURI(uint256 _tokenId) public override view returns(string memory){
+        require(_tokenId < mintCounter, "Invalid token ID!");
+
+        return string(
+            abi.encodePacked(
+                contractURIString,
+                Strings.toString(_tokenId),
+                ".json"
+            )
+        );
+    }
+
     function _baseURI() internal view virtual override returns (string memory) {
         return contractURIString;
     }
-
-    // function tokenURI(uint256 tokenId) internal view override returns(string memory){
-    //     return contractURIString;
-    // }
 
 }
 
